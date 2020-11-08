3e82a1ef89a924b644e780a4bf635c382551a99b
Carlos <carlosh97@live.com>
2020-11-07 22:09:23 -0600

    Se agrego la tabla de las portadas de los libros en home

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,7 +7,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-
+import stylesheet from '../pages/assets/styleTable.css'
 
 
 const homePage = () => {
@@ -29,8 +29,33 @@ const homePage = () => {
       <Form.Control type="text" placeholder="Busca un libro" className="mr-sm-2" />
       <Button variant="outline-primary">Buscar</Button>
     </Form>
-  </Navbar>
-        </div>
+    
+    </Navbar>
+
+<div class = "row">
+  <div class = "column">
+  <img src={logo} alt="Logo"  />
+  </div>
+  <div class = "column">
+  <img src={logo} alt="Logo"  />
+  </div>
+  <div class = "column">
+   <img src={logo} alt="Logo"  />
+  </div>
+  </div>
+   
+  <div class = "row">
+  <div class = "column">
+  <img src={logo} alt="Logo"  />
+  </div>
+  <div class = "column">
+  <img src={logo} alt="Logo"  />
+  </div>
+  <div class = "column">
+   <img src={logo} alt="Logo"  />
+  </div>
+  </div>
+</div>
     )
 }
 
