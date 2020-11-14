fe3826e8f086d9dbae17383337525ecae56a541d
diazhector98 <diazhector98@gmail.com>
2020-11-14 14:30:05 -0600

    adding reactive search

--- src/App.js
+++ src/App.js
@@ -9,19 +9,25 @@ import categoryPage from './pages/category'
 import bookPage from './pages/books'
 import test from './pages/test'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ReactiveBase } from '@appbaseio/reactivesearch';
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/signup" component={SignUpPage} />
-        <Route path="/home" component={homePage} />
-        <Route path="/category" component={categoryPage} />
-        <Route path="/books" component={bookPage} />
-        <Route path="/test" component={test} />
-      </Switch>
-    </div>
+    <ReactiveBase
+        app="good-books-ds"
+        credentials="nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d"
+      >
+      <div className="App">
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route path="/home" component={homePage} />
+          <Route path="/category" component={categoryPage} />
+          <Route path="/books" component={bookPage} />
+          <Route path="/test" component={test} />
+        </Switch>
+      </div>
+    </ReactiveBase>
   );
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -8,6 +8,12 @@ import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import stylesheet from '../pages/assets/styleTable.css'
+import {
+  ReactiveBase,
+  DataSearch,
+  SingleRange,
+  ResultCard
+} from '@appbaseio/reactivesearch';
 
 
 const homePage = () => {
@@ -18,43 +24,52 @@ const homePage = () => {
               {/* 71 y 100 */}
             <img src={logo} alt="Logo" height = "61px" width = "90" />
 
-    </Navbar.Brand>
-    <Nav className="mr-auto">
-      <Nav.Link href="/home">Home</Nav.Link>
-      <Nav.Link href="/category">Categorias</Nav.Link>
-      <Nav.Link href="/books">Mis libros</Nav.Link>
-    </Nav>
-    <Form inline>
-      <Form.Control type="text" placeholder="Busca un libro" className="mr-sm-2" />
-      <Button variant="outline-primary">Buscar</Button>
-    </Form>
-    
-    </Navbar>
+            </Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/category">Categorias</Nav.Link>
+              <Nav.Link href="/books">Mis libros</Nav.Link>
+            </Nav>
+            <Form inline>
+              <Form.Control type="text" placeholder="Busca un libro" className="mr-sm-2" />
+              <Button variant="outline-primary">Buscar</Button>
+            </Form>
+            
+            </Navbar>
 
-<div class = "row">
-  <div class = "column">
-  <img src={logo} alt="Logo"  />
-  </div>
-  <div class = "column">
-  <img src={logo} alt="Logo"  />
-  </div>
-  <div class = "column">
-   <img src={logo} alt="Logo"  />
-  </div>
-  </div>
-   
-  <div class = "row">
-  <div class = "column">
-  <img src={logo} alt="Logo"  />
-  </div>
-  <div class = "column">
-  <img src={logo} alt="Logo"  />
-  </div>
-  <div class = "column">
-   <img src={logo} alt="Logo"  />
-  </div>
-  </div>
-</div>
+            <DataSearch
+              componentId="mainSearch"
+              dataField={["original_title", "original_title.search", "authors", "authors.search"]}
+              queryFormat="and"
+              iconPosition="left"
+            />
+            <SingleRange
+              componentId="ratingsFilter"
+              dataField="average_rating"
+              title="Book Ratings"
+              data={[
+                { start: 4, end: 5, label: "★★★★ & up" },
+                { start: 3, end: 5, label: "★★★ & up" },
+                { start: 2, end: 5, label: "★★ & up" },
+                { start: 1, end: 5, label: "★ & up" },
+              ]}
+              react={{
+                and: "mainSearch"
+              }}
+            />
+            <ResultCard
+              componentId="results"
+              dataField="original_title"
+              react={{
+                "and": ["mainSearch", "ratingsFilter"]
+              }}
+              onData={(res)=>({
+                "image": res.image,
+                "title": res.original_title,
+                "description":  res.average_rating + " ★ "
+              })}
+            />
+      </div>
     )
 }
 
