ed490a077c9c8fe957cfaeab9a768403a3cad80b
Carlos <carlosh97@live.com>
2020-11-28 15:46:22 -0600

    Cambio en el nav bar
    Cambio de la barra del nav bar

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -3,70 +3,40 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { Row, Col, Button, Navbar } from "react-bootstrap"
 import Nav from 'react-bootstrap/Nav'
 import Form from 'react-bootstrap/Form'
-import logo from '../pages/assets/logo1.png'
+import logo from '../pages/assets/logo_web.png'
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import app from "../base.js"
-import Mongo from '../utils/mongo'
-import BookList from '../components/book-list'
 
 
 const ProfilePage = ({ history }) => {
     var fname, lname, username, email, uid;
     const [userInfo, setUserInfo] = useState({
-        name: "",
+        firstName: "",
+        lastName: "",
         email: "",
-        firebaseId: "",
+        userId: "",
     })
-    const [userBooks, setUserBooks] = useState([])
-    const [readingNowBooks, setReadingNowBooks] = useState([])
-    const [planningToReadBooks, setPlanningToReadBooks] = useState([])
-    const [readBooks, setReadBooks] = useState([])
-
-    const bookIdInBooks = (id, books) => {
-        for(let i = 0; i < books.length; i++) {
-            const book = books[i]
-            const {bookId} = book
-            if (id === bookId) {
-                return true
-            }
-        }
-        return false
-    }
 
     useEffect(() => {
         app.auth().onAuthStateChanged((user) => {
+            console.log({user})
             if (user != null) {
+                console.log({user})
+                const db = firebase.firestore()
+                username = user.displayName;
+                email = user.email;
                 uid = user.uid;
-                const mongo = new Mongo()
-                mongo.getUser({
-                    firebaseId: uid
-                }).then((result) => {
-                    const userData = result.data
-                    setUserInfo(userData)
-                    mongo.getUserBooks({
-                        firebaseId: uid
-                    }).then((result) => {
-                        setUserBooks(result.data)
-
-                        const books = result.data
-                        let readingBooks = []
-                        let planningBooks = []
-                        let readBooks = []
-
-                        books.forEach(book => {
-                            const {bookId} = book
-                            if (userData.planningToRead && bookIdInBooks(bookId, userData.planningToRead)) {
-                                planningBooks.push(book)
-                            }
-
-                            if (userData.readingNow && bookIdInBooks(bookId, userData.readingNow)) {
-                                readingBooks.push(book)
-                            }
-                        })
-                        setPlanningToReadBooks(planningBooks)
-                        setReadingNowBooks(readingBooks)
-
+                db.collection('users').doc(uid).get().then((info) => {
+                    console.log({info})
+                    fname = info.data().fname;
+                    lname = info.data().lname;
+                    console.log(fname, lname)
+                    setUserInfo({
+                        firstName: fname,
+                        lastName: lname,
+                        email,
+                        userId: uid
                     })
                 })
             }
@@ -104,7 +74,8 @@ const ProfilePage = ({ history }) => {
 
     
     function formatName() {
-        return userInfo.name;
+        console.log(fname)
+        return userInfo.firstName + ' ' + userInfo.lastName;
     }
 
 
@@ -122,14 +93,8 @@ const ProfilePage = ({ history }) => {
 
                 </Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link href="/home">Home</Nav.Link>
                     <Nav.Link href="/category">Categorias</Nav.Link>
-                    <Nav.Link href="/books">Mis libros</Nav.Link>
                 </Nav>
-                <Form inline>
-                    <Form.Control type="text" placeholder="Busca un libro" className="mr-sm-2" />
-                    <Button variant="outline-primary">Buscar</Button>
-                </Form>
 
             </Navbar>
             <div className="container">
@@ -147,7 +112,7 @@ const ProfilePage = ({ history }) => {
                     </div>
                     <div>
                         <label>
-                            UserId: {userInfo.firebaseId}
+                            UserId: {userInfo.userId}
                         </label>
                     </div>
                 </form>
@@ -161,13 +126,6 @@ const ProfilePage = ({ history }) => {
                         </Col>
                     </Row>
                 </div>
-
-                {/* Libros Leyendo */}
-                <BookList title="Libros Leyendo" books={readingNowBooks} />
-                {/* Libros Planeando Leer */}
-                <BookList title="Libros Planeando Leer" books={planningToReadBooks} />
-                {/* Libros Leidos */}
-                <BookList title="Libros Leidos" books={readBooks} />
             </div>
         </div>
 
