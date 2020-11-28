a8617ed4e82904032761d07beae7d3b821e25eb0
diazhector98 <diazhector98@gmail.com>
2020-11-28 16:49:56 -0600

    improve profile

--- src/components/user-book.js
+++ src/components/user-book.js
@@ -1,12 +1,32 @@
 import React from 'react'
 
+
+const containerStyles = {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 10,
+    margin: 10,
+    boxShadow: "5px 10px 18px #888888"
+}
+
+const bookDetailsContainerStyles = {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: 10, 
+    alignItems: 'flex-start'
+}
+
+
 const UserBook = ({book}) => {
-    const {title, imageLink} = book
-    console.log({book})
+    const {title, authors, imageLink} = book
     return (
-        <div>
+        <div style={containerStyles}>
             <img src={imageLink}/>
-            <h1>{title}</h1>
+            <div style={bookDetailsContainerStyles}>
+                <h3>{title}</h3>
+                <h4>{authors.join(',')}</h4>
+            </div>
+            
         </div>
     )
 }
